fb0949cb82e9cefc9bb427684ae63ff2520c55b6
devweb <devweb@dayun.cloud>
2022-03-16 21:38:14 +0800

    静态页面提交

---
+++ pages/layout/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <nav class="navbar navbar-light">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">conduit</a>
+        <ul class="nav navbar-nav pull-xs-right">
+          <li class="nav-item">
+            <!-- Add "active" class when you're on that page" -->
+            <nuxt-link class="nav-link" to="/" exact>Home</nuxt-link>
+          </li>
+            <li class="nav-item">
+             <nuxt-link class="nav-link" to="/editor">
+              <i class="ion-compose"></i>&nbsp;New Post
+             </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/settings">
+              <i class="ion-gear-a"></i>&nbsp;Settings
+             </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link  class="nav-link" to="/login">Sign in</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link  class="nav-link" to="/register">Sign up</nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <nuxt-child></nuxt-child>
+
+    <footer>
+      <div class="container">
+        <a href="/" class="logo-font">conduit</a>
+        <span class="attribution">
+          An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+        </span>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
